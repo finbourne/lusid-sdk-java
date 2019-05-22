05e2527dac3e412fb2ffe78996ae1eb988e42eb5
Rizwan Saeed <rizwan.saeed@finbourne.com>
2019-05-22 20:51:07 +0100

    Add publishing to nexus

--- publish.sh
+++ publish.sh
@@ -1,4 +1,18 @@
-sdk_version=$(cat lusid.json | jq -r '.info.version')
-mvn -f /usr/src/sdk/pom.xml versions:set -DnewVersion=$sdk_version-SNAPSHOT
+#!/bin/bash 
 
-mvn -f /usr/src/sdk/pom.xml -s /usr/src/sdk/settings.xml clean install deploy
+set -e
+
+if [[ (${#1} -eq 0) ]] ; then
+    echo 
+    echo "[ERROR] missing maven profile"
+    echo
+    exit 1
+fi
+
+profile=$1
+
+sdk_version=$(cat lusid.json | jq -r '.info.version')
+
+mvn -f ./sdk/pom.xml versions:set -DnewVersion=$sdk_version-SNAPSHOT
+mvn -f ./sdk/pom.xml -s ./sdk/settings.xml -P$profile clean install deploy -Dmaven.test.skip=true
+    
