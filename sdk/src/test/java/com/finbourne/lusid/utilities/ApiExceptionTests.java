005cbb7dc65db7db36e37d15f15eed5eb8cff9f3
Rizwan Saeed <rizwan.saeed@finbourne.com>
2019-07-04 16:20:44 +0100

    DEV-4059 Add requestId to exceptions

---
+++ sdk/src/test/java/com/finbourne/lusid/utilities/ApiExceptionTests.java
@@ -0,0 +1,46 @@
+package com.finbourne.lusid.utilities;
+
+import com.finbourne.lusid.ApiClient;
+import com.finbourne.lusid.ApiException;
+import com.finbourne.lusid.api.PortfoliosApi;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class ApiExceptionTests {
+
+    @Test
+    public void thrown_exception_tostring_contains_requestid() throws IOException, ApiException {
+
+        ApiClient apiClient = new ApiClientBuilder("secrets.json").build();
+
+        PortfoliosApi portfoliosApi = new PortfoliosApi(apiClient);
+
+        try {
+            portfoliosApi.getPortfolio("doesnt", "exist", null, null);
+        }
+        catch (ApiException e) {
+
+            String message = e.toString();
+
+            assertNotNull("Null exception message", message);
+
+            String[] parts = message.split("\\r?\\n");
+
+            assertThat(parts.length, is(greaterThanOrEqualTo(1)));
+
+            //  of the format 'LUSID request id = 000000000:AAAAAAA'
+            String[] idParts = parts[0].split(" = ");
+
+            assertThat("missing requestId", idParts.length, is(equalTo(2)));
+        }
+        catch (Exception e) {
+            fail("Unexpected exception of type " + e.getClass());
+        }
+
+
+    }
+}
