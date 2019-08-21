ee3b7d10dcd3cf54f756f4376d7e3da2626e8323
Michael McGarry <mike.mcgarry@finbourne.com>
2019-08-21 14:22:35 +0100

    Added publish.sh file back

---
+++ publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+set -e
+
+if [[ (${#1} -eq 0) ]] ; then
+    echo 
+    echo "[ERROR] missing maven profile"
+    echo
+    exit 1
+fi
+
+profile=$1
+
+sdk_version=$(cat lusid.json | jq -r '.info.version')
+
+mvn -f sdk/pom.xml versions:set -DnewVersion=$sdk_version-SNAPSHOT
+mvn -f sdk/pom.xml -s sdk/settings.xml -P$profile clean install deploy -Dmaven.test.skip=true
+    
