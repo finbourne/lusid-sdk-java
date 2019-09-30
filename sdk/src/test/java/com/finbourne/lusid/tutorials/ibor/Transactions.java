51fe6926cee0071b6d419d58f29ed36fe9def59c
Tim Barrass <tim.barrass@gmail.com>
2019-09-30 11:28:05 +0100

    DEV-4627: added tutorial/test around the transaction config api -- requires 4627 to be delivered to LUSID before this will work

--- sdk/src/test/java/com/finbourne/lusid/tutorials/ibor/Transactions.java
+++ sdk/src/test/java/com/finbourne/lusid/tutorials/ibor/Transactions.java
@@ -3,11 +3,13 @@ package com.finbourne.lusid.tutorials.ibor;
 import com.finbourne.lusid.ApiClient;
 import com.finbourne.lusid.ApiException;
 import com.finbourne.lusid.api.InstrumentsApi;
+import com.finbourne.lusid.api.SystemConfigurationApi;
 import com.finbourne.lusid.api.TransactionPortfoliosApi;
 import com.finbourne.lusid.model.*;
 import com.finbourne.lusid.utilities.ApiClientBuilder;
 import com.finbourne.lusid.utilities.CredentialsSource;
 import com.finbourne.lusid.utilities.InstrumentLoader;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -17,6 +19,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.finbourne.lusid.utilities.TestDataUtilities.*;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 
 public class Transactions {
@@ -24,6 +27,7 @@ public class Transactions {
     private static InstrumentsApi instrumentsApi;
     private static TransactionPortfoliosApi transactionPortfoliosApi;
     private static List<String> instrumentIds;
+    private static SystemConfigurationApi systemConfigurationApi;
 
     @BeforeClass
     public static void setUp() throws Exception
@@ -36,6 +40,108 @@ public class Transactions {
         //  ensure instruments are created and exist in LUSID
         InstrumentLoader instrumentLoader = new InstrumentLoader(instrumentsApi);
         instrumentIds = instrumentLoader.loadInstruments();
+
+        systemConfigurationApi = new SystemConfigurationApi(apiClient);
+    }
+
+    @Test
+    public void list_set_and_create_transaction_types() throws ApiException
+    {
+        TransactionSetConfigurationData def = systemConfigurationApi.listConfigurationTransactionTypes();
+
+        //  now create an alternative Buy, and add it to the default configuration
+        TransactionConfigurationTypeAlias alias = new TransactionConfigurationTypeAlias();
+        alias.type("NewBuy");
+        alias.description("Description");
+        alias.transactionClass("Class");
+        alias.transactionGroup("Group");
+        List<TransactionConfigurationTypeAlias> aliases = new LinkedList<TransactionConfigurationTypeAlias>();
+        aliases.add(alias);
+        TransactionConfigurationMovementDataRequest movement = new TransactionConfigurationMovementDataRequest();
+        movement.movementTypes(TransactionConfigurationMovementDataRequest.MovementTypesEnum.COMMITMENT);
+        movement.side(TransactionConfigurationMovementDataRequest.SideEnum.SIDE1);
+        movement.direction(1);
+        List<TransactionConfigurationMovementDataRequest> movements = new LinkedList<TransactionConfigurationMovementDataRequest>();
+        movements.add(movement);
+        TransactionConfigurationDataRequest newBuy = new TransactionConfigurationDataRequest();
+        newBuy.aliases(aliases);
+        newBuy.movements(movements);
+
+        systemConfigurationApi.createConfigurationTransactionType(newBuy);
+
+        //    there should be one more entry in the updated configuration
+        TransactionSetConfigurationData updated = systemConfigurationApi.listConfigurationTransactionTypes();
+        Assert.assertThat(updated.getTransactionConfigs().size() - def.getTransactionConfigs().size(), is(1));
+
+        //    now let's use our Buy, and replace the default configuration
+        //    REALISE THAT THIS IS A DESTRUCTIVE OPERATION -- you can't trivially recover the original defaults
+        //    unless you have them stored down -- we'll restore them afterwards
+        TransactionSetConfigurationDataRequest request = new TransactionSetConfigurationDataRequest();
+        request.addTransactionConfigRequestsItem(newBuy);
+        systemConfigurationApi.setConfigurationTransactionTypes(request);
+
+        //  there should be one entry in the updated configuration
+        updated = systemConfigurationApi.listConfigurationTransactionTypes();
+        Assert.assertThat(updated.getTransactionConfigs().size(), is(1));
+
+        //  finally, let's restore the original default configurations
+        TransactionSetConfigurationDataRequest defRequest = convertToRequest(def);
+        systemConfigurationApi.setConfigurationTransactionTypes(defRequest);
+
+        //  and confirm that the number of transaction configurations is as expected
+        updated = systemConfigurationApi.listConfigurationTransactionTypes();
+        Assert.assertThat(updated.getTransactionConfigs().size(), is(def.getTransactionConfigs().size()));
+    }
+
+    private static TransactionSetConfigurationDataRequest convertToRequest(TransactionSetConfigurationData d)
+    {
+        return new TransactionSetConfigurationDataRequest()
+                .transactionConfigRequests(
+                        d.getTransactionConfigs().stream().map(c -> convertToRequest(c)).collect(Collectors.toList()));
+    }
+
+    private static TransactionConfigurationDataRequest convertToRequest(TransactionConfigurationData d)
+    {
+        TransactionConfigurationDataRequest request = new TransactionConfigurationDataRequest();
+        request.aliases(d.getAliases());
+        request.movements(d.getMovements().stream().map(m -> convertToRequest(m)).collect(Collectors.toList()));
+        request.properties(d.getProperties());
+
+        return request;
+    }
+
+    private static TransactionConfigurationMovementDataRequest convertToRequest(TransactionConfigurationMovementData d)
+    {
+        TransactionConfigurationMovementDataRequest request = new TransactionConfigurationMovementDataRequest();
+        request.movementTypes(convertToRequest(d.getMovementTypes()));
+        request.side(convertToRequest(d.getSide()));
+        request.direction(d.getDirection());
+        request.properties(d.getProperties());
+        request.mappings(d.getMappings().stream().map(m -> convertToRequest(m)).collect(Collectors.toList()));
+
+        return request;
+    }
+
+    private static TransactionPropertyMappingRequest convertToRequest(TransactionPropertyMapping d)
+    {
+         TransactionPropertyMappingRequest request = new TransactionPropertyMappingRequest();
+         request.propertyKey(d.getPropertyKey());
+         request.mapFrom(d.getMapFrom());
+         request.setTo(d.getSetTo());
+
+         return request;
+    }
+
+    private static TransactionConfigurationMovementDataRequest.MovementTypesEnum convertToRequest(
+            TransactionConfigurationMovementData.MovementTypesEnum original)
+    {
+        return TransactionConfigurationMovementDataRequest.MovementTypesEnum.fromValue(original.getValue());
+    }
+
+    private static TransactionConfigurationMovementDataRequest.SideEnum convertToRequest(
+            TransactionConfigurationMovementData.SideEnum original)
+    {
+        return TransactionConfigurationMovementDataRequest.SideEnum.fromValue(original.toString());
     }
 
     @Test
