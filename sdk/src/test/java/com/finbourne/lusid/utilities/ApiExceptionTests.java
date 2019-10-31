8867ff74f9fec7b6b142b72ba458de9ff3c3b29e
Yemmy Chan <yemmy.chan@finbourne.com>
2019-10-31 16:55:19 +0000

    TR-282: Fix API func

--- sdk/src/test/java/com/finbourne/lusid/utilities/ApiExceptionTests.java
+++ sdk/src/test/java/com/finbourne/lusid/utilities/ApiExceptionTests.java
@@ -20,7 +20,7 @@ public class ApiExceptionTests {
         PortfoliosApi portfoliosApi = new PortfoliosApi(apiClient);
 
         try {
-            portfoliosApi.getPortfolio("doesnt", "exist", null, null);
+            portfoliosApi.getPortfolio("doesnt", "exist", null, null, null);
         }
         catch (ApiException e) {
 
