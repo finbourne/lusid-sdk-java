4b983adb5aecf8eda418ee2a4a6d98b05003f242
Concourse <concourse@finbourne.com>
2021-08-11 13:16:51 +0000

    Update SDK 0.11.3377 [publish]

--- sdk/src/main/java/com/finbourne/lusid/ApiClient.java
+++ sdk/src/main/java/com/finbourne/lusid/ApiClient.java
@@ -56,7 +56,7 @@ import com.finbourne.lusid.auth.OAuthFlow;
 
 public class ApiClient {
 
-    private String basePath = "https://fbn-prd.lusid.com/api";
+    private String basePath = "http://local-unit-test-server.lusid.com:39653";
     private boolean debugging = false;
     private Map<String, String> defaultHeaderMap = new HashMap<String, String>();
     private String tempFolderPath = null;
@@ -148,7 +148,7 @@ public class ApiClient {
     /**
      * Set base path
      *
-     * @param basePath Base path of the URL (e.g https://fbn-prd.lusid.com/api
+     * @param basePath Base path of the URL (e.g http://local-unit-test-server.lusid.com:39653
      * @return An instance of OkHttpClient
      */
     public ApiClient setBasePath(String basePath) {
