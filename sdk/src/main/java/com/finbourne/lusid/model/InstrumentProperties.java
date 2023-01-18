621f98cabc29920baed48e93f9b5e8ef1ada9f35
Concourse <concourse@finbourne.com>
2023-01-18 08:51:04 +0000

    Update SDK 0.11.5151 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/InstrumentProperties.java
+++ sdk/src/main/java/com/finbourne/lusid/model/InstrumentProperties.java
@@ -134,10 +134,10 @@ public class InstrumentProperties {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
