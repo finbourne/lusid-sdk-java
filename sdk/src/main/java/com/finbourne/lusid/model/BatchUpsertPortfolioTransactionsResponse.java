549d62b40f1c74270dbccd45064e1d782ad7e652
Concourse <concourse@finbourne.com>
2023-05-03 12:07:44 +0000

    Update SDK 1.0.131 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/BatchUpsertPortfolioTransactionsResponse.java
+++ sdk/src/main/java/com/finbourne/lusid/model/BatchUpsertPortfolioTransactionsResponse.java
@@ -150,10 +150,10 @@ public class BatchUpsertPortfolioTransactionsResponse {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
