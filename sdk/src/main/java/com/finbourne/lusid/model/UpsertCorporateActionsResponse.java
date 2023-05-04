3f049d556287892980033d2380b1e3e8680ef9d7
Concourse <concourse@finbourne.com>
2023-05-04 08:33:11 +0000

    Update SDK 1.0.133 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/UpsertCorporateActionsResponse.java
+++ sdk/src/main/java/com/finbourne/lusid/model/UpsertCorporateActionsResponse.java
@@ -142,10 +142,10 @@ public class UpsertCorporateActionsResponse {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
