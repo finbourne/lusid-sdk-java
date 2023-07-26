ffa13ef4c89c1c3e5ec54da64b2b7d891c07b1c4
Concourse <concourse@finbourne.com>
2023-07-26 19:44:29 +0000

    Update SDK 1.0.384 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/VersionSummaryDto.java
+++ sdk/src/main/java/com/finbourne/lusid/model/VersionSummaryDto.java
@@ -121,10 +121,10 @@ public class VersionSummaryDto {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
