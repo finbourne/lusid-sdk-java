139d822451d15206f1a26a61344c3e1801606a20
Rizwan Saeed <rizwan.saeed@finbourne.com>
2023-07-28 18:46:04 +0100

    Update BitemporalTest.java

--- sdk/src/test/java/com/finbourne/lusid/tutorials/ibor/BitemporalTest.java
+++ sdk/src/test/java/com/finbourne/lusid/tutorials/ibor/BitemporalTest.java
@@ -34,9 +34,9 @@ public class BitemporalTest {
     @BeforeClass
     public static void setUp() throws Exception {
         ApiConfiguration apiConfiguration = new ApiConfigurationBuilder().build(CredentialsSource.credentialsFile);
-        ApiClient apiClient = new ApiClientBuilder().build(apiConfiguration);
+        ApiClient apiClient = new ApiClientBuilder().build(apiConfiguration, 30, 30);
 
-        transactionPortfoliosApi = new TransactionPortfoliosApi(apiClient, 30, 30);
+        transactionPortfoliosApi = new TransactionPortfoliosApi(apiClient);
 
         testDataUtilities = new TestDataUtilities(transactionPortfoliosApi);
 
