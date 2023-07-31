037c23050e038652f79d313a2b271db4699891e8
Concourse <concourse@finbourne.com>
2023-07-31 21:22:16 +0000

    Update SDK 1.0.396 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/ComplianceRuleResponse.java
+++ sdk/src/main/java/com/finbourne/lusid/model/ComplianceRuleResponse.java
@@ -304,10 +304,10 @@ public class ComplianceRuleResponse {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
