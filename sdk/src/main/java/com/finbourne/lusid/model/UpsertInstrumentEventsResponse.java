741892239308601ec8a50934072cde7f06fdedbf
Concourse <concourse@finbourne.com>
2023-08-04 17:59:39 +0000

    Update SDK 1.0.417 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/UpsertInstrumentEventsResponse.java
+++ sdk/src/main/java/com/finbourne/lusid/model/UpsertInstrumentEventsResponse.java
@@ -142,10 +142,10 @@ public class UpsertInstrumentEventsResponse {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
