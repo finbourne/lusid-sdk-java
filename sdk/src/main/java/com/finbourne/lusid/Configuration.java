7e820f5a99c5abedc21061ab83095943087e3c2c
Concourse <concourse@finbourne.com>
2023-09-28 08:25:25 +0000

    Updates for v2.0.82-SNAPSHOT

--- sdk/src/main/java/com/finbourne/lusid/Configuration.java
+++ sdk/src/main/java/com/finbourne/lusid/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.lusid;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.76-SNAPSHOT";
+    public static final String VERSION = "2.0.82-SNAPSHOT";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
