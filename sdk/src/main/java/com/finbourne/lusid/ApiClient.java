f4e33537ead489c6a0e6512284dd9fc0b05e9207
Concourse <concourse@finbourne.com>
2023-10-19 17:12:50 +0000

    Updates for v2.0.155-SNAPSHOT

--- sdk/src/main/java/com/finbourne/lusid/ApiClient.java
+++ sdk/src/main/java/com/finbourne/lusid/ApiClient.java
@@ -208,7 +208,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2.0.154-SNAPSHOT/java");
+        setUserAgent("OpenAPI-Generator/2.0.155-SNAPSHOT/java");
 
         authentications = new HashMap<String, Authentication>();
     }
--- sdk/src/main/java/com/finbourne/lusid/Configuration.java
+++ sdk/src/main/java/com/finbourne/lusid/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.lusid;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.154-SNAPSHOT";
+    public static final String VERSION = "2.0.155-SNAPSHOT";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
