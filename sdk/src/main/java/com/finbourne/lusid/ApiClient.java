8788c863597965a35efa35e93721f21b2188dff7
Concourse <concourse@finbourne.com>
2024-04-02 23:22:03 +0000

    Updates for v2.0.640

--- sdk/src/main/java/com/finbourne/lusid/ApiClient.java
+++ sdk/src/main/java/com/finbourne/lusid/ApiClient.java
@@ -208,7 +208,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2.0.636/java");
+        setUserAgent("OpenAPI-Generator/2.0.640/java");
 
         authentications = new HashMap<String, Authentication>();
     }
--- sdk/src/main/java/com/finbourne/lusid/Configuration.java
+++ sdk/src/main/java/com/finbourne/lusid/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.lusid;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.636";
+    public static final String VERSION = "2.0.640";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
