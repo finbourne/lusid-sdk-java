1f1e640f78eb670ada4fcb3f6f9ee2df59b490d7
Concourse <concourse@finbourne.com>
2024-08-02 20:21:35 +0000

    Updates for v2.0.959

--- sdk/src/main/java/com/finbourne/lusid/Configuration.java
+++ sdk/src/main/java/com/finbourne/lusid/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.lusid;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.957";
+    public static final String VERSION = "2.0.959";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
