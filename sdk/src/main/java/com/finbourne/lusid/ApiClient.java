d5669342c5ee6af5b81baf6b983e8385b8f0440c
Concourse <concourse@finbourne.com>
2025-05-27 15:51:29 +0000

    Updates for v2.0.1453

--- sdk/src/main/java/com/finbourne/lusid/ApiClient.java
+++ sdk/src/main/java/com/finbourne/lusid/ApiClient.java
@@ -212,7 +212,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2.0.1452/java");
+        setUserAgent("OpenAPI-Generator/2.0.1453/java");
 
         authentications = new HashMap<String, Authentication>();
     }
